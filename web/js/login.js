115beab8876ab66881a777d506a4285950a8ad12
Jose Carlos Gutierrez <joseco48@gmail.com>
2020-12-07 19:25:26 -0400

    Version Inicial

---
+++ web/js/login.js
@@ -0,0 +1,98 @@
+(function(){
+    
+    
+    
+})();
+
+(function(){
+    
+    if(sessionStorage.getItem("userInSession")){
+        document.location.href = "index.html";
+        return;
+    }
+    
+    document.body.style.display = "block"
+
+    document.querySelector("#btn-login").addEventListener('click',(e) =>{
+        e.preventDefault();
+        
+        
+        
+        const [ validationUsername, validationPassword, msgError ] =
+                    document.querySelectorAll("#validation-username, #validation-password, #msg-error-login");
+        /*
+        const elementos = document.querySelectorAll("#validation-username, #validation-password,, #msg-error-login");
+        const validationUsername = elementos[0];
+        const validationPassword = elementos[1];
+        const msgError = elementos[2];
+        */
+       
+       /*
+        const validationUsername = document.getElementById("validation-username");
+        const validationPassword = document.querySelector("#validation-password");
+        const msgError = document.querySelector("#msg-error-login");
+        */
+
+        validationUsername.style.display = "none";
+        validationPassword.style.display = "none";
+        msgError.style.display = "none";
+
+        const inputUsername = document.querySelector("#inputUsername");
+        const inputPassword = document.querySelector("#inputPassword");
+        let hasError = false;
+        if(inputUsername.value == ""){
+            hasError = true;
+            validationUsername.style.display = "block";
+        }
+        if(inputPassword.value == ""){
+            hasError = true;
+            validationPassword.style.display = "block";
+        }
+
+        if(hasError){
+            return;
+        }
+
+        const usuario = {
+            "username" : inputUsername.value,
+            "password" : inputPassword.value
+        }
+        fetch('api/usuario/login', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json', //MimeType
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(usuario)
+        }).then((response) =>{
+            return response.json();
+        }).then((data) => {
+            
+            if(!data.isOK){
+                msgError.innerHTML = data.message;
+                msgError.style.display = "block"
+                return;
+            }
+            sessionStorage.setItem("userInSession" , data.message);
+
+            document.location.href = "index.html";
+
+        });
+
+        /*
+        var elementos = document.querySelectorAll("#validation-username, #validation-password");
+        for(let i =0 ; i < elementos.length; i++){
+            elementos[i].style.display = "none";
+        }
+
+        var elementos = document.querySelectorAll("#validation-username, #validation-password");
+        for(let i in elementos){
+            elementos[i].style.display = "none";
+        }
+        */
+
+    })
+
+
+
+})();
